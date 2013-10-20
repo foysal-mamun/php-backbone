522927b735cd493c3e5d072f22eb5c50929b1a22
lifo <lifo.java@yahoo.com>
2013-10-20 21:35:02 +0600

    fixed model

--- js/custom.js
+++ js/custom.js
@@ -7,8 +7,7 @@
  */
 
 var MessageModel = Backbone.Model.extend({
-    urlRoot : 'messages.php',
-    message : "test"
+    urlRoot : 'messages.php'
 });
 
 var MessageView = Backbone.View.extend({
@@ -16,7 +15,7 @@ var MessageView = Backbone.View.extend({
     template:_.template($('#tpl-hello-backbone').html()),
 
     render:function (eventName) {
-        $(this.el).html(this.template(this.model));
+        $(this.el).html(this.template(this.model.toJSON()));
         return this;
     }
 });
